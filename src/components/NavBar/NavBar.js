611892a505dc6483b3ab51b5ef245427871d46a8
rakibul58 <rhrahi14@gmail.com>
2022-10-10 23:46:32 +0600

    navbar added

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,14 +1,31 @@
-import React from 'react';
-import { Bars3Icon } from '@heroicons/react/24/solid'
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import { Link } from "react-router-dom";
 const NavBar = () => {
-    return (
-        <nav>
-            <Link to='/'>Home</Link>
-            <Link to='/statistics'>Statistics</Link>
-            <Link to='/blog'>Blog</Link>
-        </nav>
-    );
+  const [open, setOpen] = useState(false);
+  return (
+    <nav className="bg-green-500 flex w-full justify-between py-2 items-center">
+      <h1 className="lg:ml-28 text-2xl ml-2 font-bold text-white">Quiz Master</h1>
+      <div onClick={() => setOpen(!open)} className="h-6 w-6 md:hidden mr-2 text-white">
+        {open ? <XMarkIcon /> : <Bars3Icon />}
+      </div>
+      <div
+        className={`bg-green-500 text-lg font-semibold text-white p-4 lg:mr-28 absolute flex flex-col justify-center md:flex-row w-full md:w-fit md:static duration-500 ease-in ${
+          open ? "top-12" : "top-[-120px]"
+        }`}
+      >
+        <Link className="md:ml-7 ml-0 hover:text-gray-300" to="/">
+          Home
+        </Link>
+        <Link className="md:ml-7 ml-0 hover:text-gray-300" to="/statistics">
+          Statistics
+        </Link>
+        <Link className="md:ml-7 ml-0 hover:text-gray-300" to="/blog">
+          Blog
+        </Link>
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
