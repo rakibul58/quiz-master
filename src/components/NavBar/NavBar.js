ad1373c595d60f75b4a5b95c0f176769d24d6e99
rakibul58 <rhrahi14@gmail.com>
2022-10-10 21:11:45 +0600

    components and react router added'

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <div>
+            <h2>This is Navbar</h2>
+        </div>
+    );
+};
+
+export default NavBar;
---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Statistics = () => {
+    return (
+        <div>
+            <h2>This is statistic</h2>
+        </div>
+    );
+};
+
+export default Statistics;
