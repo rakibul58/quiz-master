f45925ed68b4da206668e08963d67b0ce138a3b3
rakibul58 <rhrahi14@gmail.com>
2022-10-10 20:56:28 +0600

    hero icons added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
-
+import { Bars3Icon } from '@heroicons/react/24/solid'
 function App() {
   return (
     <div className="App">
+      <Bars3Icon className='h-6 w-6'></Bars3Icon>
       <h1>Tailwind css</h1>
     </div>
   );
